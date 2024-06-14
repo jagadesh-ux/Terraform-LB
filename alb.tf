2c53ecc47ca38631ce36d86fa3ac9170259782b7
gampasivakumar4422 <138592547+gampasivakumar4422@users.noreply.github.com>
2024-06-14 09:47:35 +0530

    Update and rename nlb.tf to alb.tf

--- alb.tf
+++ alb.tf
@@ -1,15 +1,15 @@
-resource "aws_lb" "network" {
+resource "aws_lb" "application" {
   name               = var.aws_lb
   internal           = false
   load_balancer_type =  var.load_balancer_type
   security_groups    = [var.vpc_security_group_id]
   subnets            = var.subnet_id[*]
   tags = {
-    Name = "network-lb"
+    Name = "application-lb"
   }
 }
 
-resource "aws_lb_target_group" "network" {
+resource "aws_lb_target_group" "application" {
   name     = var.aws_lb_target_group
   port     = 80
   protocol = "HTTP"
@@ -27,23 +27,23 @@ resource "aws_lb_target_group" "network" {
   }
 }
 
-resource "aws_lb_listener" "network" {
-  load_balancer_arn = aws_lb.network.arn
+resource "aws_lb_listener" "application" {
+  load_balancer_arn = aws_lb.application.arn
   port              = 80
   protocol          = "HTTP"
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.network.arn
+    target_group_arn = aws_lb_target_group.application.arn
   }
 }
 
 resource "aws_lb_target_group_attachment" "web" {
-  target_group_arn = aws_lb_target_group.network.arn
+  target_group_arn = aws_lb_target_group.application.arn
   target_id        = aws_instance.web.id
   port             = 80
 }
 resource "aws_lb_target_group_attachment" "webserver" {
-  target_group_arn = aws_lb_target_group.network.arn
+  target_group_arn = aws_lb_target_group.application.arn
   target_id        = aws_instance.webserver.id
   port             = 80
 }
